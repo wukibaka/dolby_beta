ae8d50d04e63cb57597e83ede63ccbf9721464da
lin <83531505+luoxingran@users.noreply.github.com>
2022-07-15 22:28:10 +0800

    Update ListentogetherHook.java

--- app/src/main/java/com/raincat/dolby_beta/hook/ListentogetherHook.java
+++ app/src/main/java/com/raincat/dolby_beta/hook/ListentogetherHook.java
@@ -10,7 +10,10 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class ListentogetherHook {
     public ListentogetherHook(Context context, int versionCode) {
-        if (versionCode > 8007090) {
+        if (versionCode > 8007091) {
+            findAndHookMethod(findClass("com.netease.cloudmusic.module.listentogether.y", context.getClassLoader()),
+                    "v", XC_MethodReplacement.returnConstant(true));
+        }else if (versionCode > 8007090) {
             findAndHookMethod(findClass("com.netease.cloudmusic.module.listentogether.x", context.getClassLoader()),
                     "v", XC_MethodReplacement.returnConstant(true));
         }else if (versionCode == 8007090) {
